d4fc8e068ce234bc9e063d1ee4fd529840f39a0f
TomcZQ <tomdevpro38@gmail.com>
2025-09-26 16:57:19 +0200

    feat :
    implemented DB access from venv

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from sqlalchemy import text
+from src.app.database.config import settings
+from src.app.database.db import engine
 
-app = FastAPI(title="GymApp")
+app = FastAPI(title=settings.PROJECT_NAME)
 
 @app.get("/healthz")
 def healthz():
@@ -10,3 +13,8 @@ def healthz():
 def root():
 	return {"message": "Hello from FastAPI"}
 
+@app.get("/health/db")
+def health_db():
+    with engine.connect() as conn:
+        conn.execute(text("SELECT 1"))
+    return {"db": "ok"}
